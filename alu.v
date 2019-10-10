f15631c0cdbd61c6c76ea926efd3440a29c37dde
nelson <nelson.cook98@gmail.com>
2019-10-11 11:25:31 +1300

    Got multiplication working

--- alu.v
+++ alu.v
@@ -10,15 +10,15 @@ module alu#(parameter BITS = 21)
 				input [1:0] opcode,
 				input clock, computestrobe,
 				output reg signed[BITS-1:0] result, //21-bit reg for signed int +999*999 to -999*999
-				output reg ovf
+				output reg ovf,
+				output reg remainderbitmask // Where values are 1, the mux should display that as remainder?
 				);
 				
-	reg signed [BITS-1:0] tempSum, shiftedA;
+	reg signed [BITS-1:0] tempSum, shiftedA, extendedB;
 	integer i;
 	always @(posedge clock) begin
 		if (computestrobe) begin 
-			tempSum = 0;
-			shiftedA = regA;
+
 			case(opcode)
 			`add : begin 
 				result = regA + regB;
@@ -28,18 +28,25 @@ module alu#(parameter BITS = 21)
 			end
 			
 			`multiply : begin //Doesn't seem to handle 2's compleemnt negative numbers right
-				for (i = 0; i < 11; i = i+1) begin
-					//tempSum =(regB[i] == 1'b1)? tempSum + regA : tempSum;
-					if (regB[i] == 1'b1) begin
-						tempSum = tempSum + shiftedA;
+			
+				tempSum = 0;
+
+				shiftedA = {{10{regA[10]}}, {regA}};
+				extendedB = {{10{regB[10]}}, {regB}};
+				
+				
+					for (i = 0; i < 21; i = i+1) begin
+						//tempSum =(regB[i] == 1'b1)? tempSum + regA : tempSum;
+						if (extendedB[i] == 1'b1) begin
+							tempSum = tempSum + shiftedA;
+						end
+						shiftedA = shiftedA << 1;
 					end
-					shiftedA = shiftedA << 1;
-				end
 				result = tempSum;
 			end
-			
+				
 			default : begin
-			result = 0;
+				result = 0;
 			end
 			
 			
--- simulation/modelsim/ALUtest.do
+++ simulation/modelsim/ALUtest.do
@@ -68,3 +68,28 @@ for { set i -3 } { $i<=4 } { incr i } { #subtrcheck
 
 
 echo "PASS SUBTR TESTS"
+
+
+
+force -freeze sim:/alu/opcode 10 0
+
+for { set i -3 } { $i<=4 } { incr i } { #timescheck    
+	# Convert to 32 binary digits, store result in "i_bin" variable
+	# I don't know how this fucking works fuck    
+	binary scan [binary format I $i] B32 i_bin  
+	
+	for { set j -3 } { $j<=4 } { incr j } {
+	   
+	   binary scan [binary format I $j] B32 j_bin
+	   
+	   # Set compute strobe high for 1 cycle
+	   force -freeze sim:/alu/computestrobe 1 0 -cancel 100 
+	   force -freeze sim:/alu/regA $i_bin 0 
+	   force -freeze sim:/alu/regB $j_bin 0    
+	   run
+	   if {[examine -radix decimal sim:/alu/result] != $i * $j} { 
+		  echo "FAIL SUBTR TEST $i * $j"       
+		  abort
+	   }
+	}
+}
